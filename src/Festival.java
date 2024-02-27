9b392e24c43ab09fb062df44dc468a7819938a68
mpenalvart1@educacion.navarra.es <mpenalvart1@educacion.navarra.es>
2024-02-27 15:37:55 +0100

    Haciendo las bases de Festival.java

--- src/Festival.java
+++ src/Festival.java
@@ -3,8 +3,53 @@ import java.time.LocalDate;
 import java.util.HashSet;
 
 /**
- * Enumeración que representa los meses del año.
+ * Un objeto de esta clase almacena los datos de un
+ * festival.
+ * Todo festival tiene un nombre, se celebra en un lugar
+ * en una determinada fecha, dura una serie de d�as y
+ * se engloba en un conjunto determinado de estilos
+ *
  */
+public class Festival {
+    private final String nombre;
+    private final String lugar;
+    private final LocalDate fechaInicio;
+    private final int duracion;
+    private final HashSet<Estilo> estilos;
 
 
+    public Festival(String nombre, String lugar, LocalDate fechaInicio,
+                    int duracion, HashSet<Estilo> estilos) {
+        this.nombre = nombre;
+        this.lugar = lugar;
+        this.fechaInicio = fechaInicio;
+        this.duracion = duracion;
+        this.estilos = estilos;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public HashSet<Estilo> getEstilos() {
+        return estilos;
+    }
+
+    public void addEstilo(Estilo estilo) {
+        this.estilos.add(estilo);
+
+    }
 
